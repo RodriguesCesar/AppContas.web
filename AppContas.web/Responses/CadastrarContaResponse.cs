b06c75f25f1421d73707f243be889fa0935a60b8
cesar.sousa <sousa.rodrigues.cesar@gmail.com>
2023-01-08 19:11:08 -0300

    Criando componente Cadastrar Conta

---
+++ AppContas.web/Responses/CadastrarContaResponse.cs
@@ -0,0 +1,10 @@
+﻿using AppContas.Web.Responses;
+
+namespace AppContas.web.Responses
+{
+    public class CadastrarContaResponse
+    {
+        public string? Mensagem { get; set; }
+        public ContaResponse? Conta { get; set; }
+    }
+}
